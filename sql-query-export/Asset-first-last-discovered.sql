9d4e43ceead8c0142afb8d5aa84397779ff17d09
Steve Passino <spassino-r7@users.noreply.github.com>
2022-11-01 13:25:09 -0400

    Adding Asset-first-last-discovered.sql

---
+++ sql-query-export/Asset-first-last-discovered.sql
@@ -0,0 +1,6 @@
+-- Asset report with first and last discovered dates.
+-- Provides asset IP, hostname, mac_address, first date discovered, and last date discovered.
+-- Copy the SQL query below
+SELECT da.ip_address, da.host_name, da.mac_address, fad.first_discovered, fad.last_discovered
+FROM fact_asset_discovery fad
+ JOIN dim_asset da USING (asset_id)
