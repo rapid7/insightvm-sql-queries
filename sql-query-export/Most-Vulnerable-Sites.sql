7a112130086d49fffc524f1178f4d15e19afbf8a
Holly Wilsey <43147347+hwilsey-r7@users.noreply.github.com>
2022-11-28 11:10:48 -0500

    Create Most-Vulnerable-Sites.sql

---
+++ sql-query-export/Most-Vulnerable-Sites.sql
@@ -0,0 +1,8 @@
+-- List most vulnerable sites in descending order based on risk score per asset
+-- Copy the SQL query below
+
+SELECT ds.name AS site, assets, riskscore,
+(CASE riskscore WHEN 0 THEN NULL ELSE riskscore END) / (CASE assets WHEN 0 THEN NULL ELSE assets END) AS "RiskPerAsset"
+FROM fact_site
+JOIN dim_site ds USING (site_id)
+ORDER BY "RiskPerAsset" DESC
