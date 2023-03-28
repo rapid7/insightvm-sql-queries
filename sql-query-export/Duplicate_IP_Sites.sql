7136569732fdfd5aaecbd5572476408977c0b349
cbaudier-r7 <110676038+cbaudier-r7@users.noreply.github.com>
2023-03-28 14:58:46 -0400

    Update Duplicate_IP_Sites.sql

--- sql-query-export/Duplicate_IP_Sites.sql
+++ sql-query-export/Duplicate_IP_Sites.sql
@@ -1,6 +1,5 @@
 
---  Duplicate IPs across sites.  Note that this will likely include global (correlated) assets if you have asset linking
-
+-- Duplicate IPs across sites.  Note that this will likely include global (correlated) assets if you have asset linking
 -- Copy the SQL query below
 
 SELECT ip_address AS "IP Address", sites AS "Sites IP Address Appears"
