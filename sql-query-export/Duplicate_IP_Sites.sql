e1f56afdf23678a34e9b54739d60106a7f714c93
Keith Marburger <keith_marburger@rapid7.com>
2023-03-28 11:30:13 -0700

    Duplicate_IP_Sites.sql

---
+++ sql-query-export/Duplicate_IP_Sites.sql
@@ -0,0 +1,7 @@
+
+--  Duplicate IPs across sites.  Note that this will likely include global (correlated) assets if you have asset linking
+
+-- Copy the SQL query below
+
+SELECT ip_address AS "IP Address", sites AS "Sites IP Address Appears"
+FROM dim_asset where sites like '%,%';
