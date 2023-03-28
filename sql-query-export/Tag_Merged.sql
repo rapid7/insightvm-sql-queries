a926c6f3a99a4e9c79093e397cacfea7c98fdd62
Keith Marburger <keith_marburger@rapid7.com>
2023-03-28 11:14:26 -0700

    Tag_Merged.sql

---
+++ sql-query-export/Tag_Merged.sql
@@ -0,0 +1,49 @@
+--  Tags and merged
+
+-- Report contains the following:
+-- IP Address
+-- Port, Service
+-- Protocol
+-- Report Date
+-- OS
+-- Tag_Name
+
+
+SELECT 
+  da.ip_address, 
+  das.port,
+  ds.name AS service,
+  dp.name AS protocol, 
+now() AS 
+  report_date,
+  dos.name AS os,
+  array_to_string(array_agg(dt.tag_name),',') as 
+    tag_name
+FROM dim_asset_service das
+JOIN 
+  dim_service ds 
+USING (service_id)
+JOIN 
+  dim_protocol dp 
+USING (protocol_id)
+JOIN 
+  dim_asset da 
+USING (asset_id)
+JOIN 
+  dim_operating_system dos 
+USING (operating_system_id)
+LEFT OUTER JOIN 
+  dim_tag_asset dta 
+USING (asset_id)
+LEFT OUTER JOIN 
+  dim_tag dt 
+USING (tag_id)
+GROUP BY 
+  da.ip_address, 
+  das.port,
+  ds.name,
+  dp.name,
+  dos.name
+ORDER BY 
+  da.ip_address,
+  ds.name
