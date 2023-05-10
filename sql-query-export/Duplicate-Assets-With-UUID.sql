ad87fc5ab72854122d0049ccc4dd4164d0b083da
Keith Marburger <keith_marburger@rapid7.com>
2023-05-10 15:37:32 -0700

    Update Duplicate-Assets-With-UUID.sql

--- sql-query-export/Duplicate-Assets-With-UUID.sql
+++ sql-query-export/Duplicate-Assets-With-UUID.sql
@@ -1,4 +1,6 @@
 -- Duplicate Asset with UUID
+-- The query will provide the following:   asset_id, uuid, source, ip_address, mac_address, host_name, sites
+
 -- Copy the SQL query below
 
 SELECT dauid.asset_id, MAX(dauid.unique_id) as uuid, MAX(dauid.source) AS source, da.ip_address, da.mac_address, da.host_name, da.sites 
