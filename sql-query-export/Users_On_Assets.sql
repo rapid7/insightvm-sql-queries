45bb698f5f3bbce88692d74549d866f2c9cf6cae
Keith Marburger <keith_marburger@rapid7.com>
2023-06-01 17:44:57 -0700

    Create Users_On_Assets.sql

---
+++ sql-query-export/Users_On_Assets.sql
@@ -0,0 +1,14 @@
+-- list users found during scans
+
+--Query Will Provide:
+
+--  name
+--	full_name
+--  ip_address
+-- 	host_name
+
+-- Copy the SQL query below
+
+SELECT daua.name, daua.full_name, da.ip_address, da.host_name
+FROM dim_asset_user_account daua
+JOIN dim_asset da using (asset_id)
